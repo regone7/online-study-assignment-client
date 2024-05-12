fd3d83ca373bdd05b480bae4849d32192f55ab80
regone7 <regone12345@gmail.com>
2024-05-12 17:59:16 +0600

    delete function

--- src/components/FilterAllAssignment.jsx
+++ src/components/FilterAllAssignment.jsx
@@ -2,11 +2,42 @@ import { MdShutterSpeed } from "react-icons/md";
 import { IoIosBookmarks } from "react-icons/io";
 import { GrLinkNext } from "react-icons/gr";
 import { Link } from 'react-router-dom';
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../provider/AuthProvider";
+import axios from "axios";
+import "react-datepicker/dist/react-datepicker.css";
+import toast, { Toaster } from 'react-hot-toast';
 
 const FilterAllAssignment = ({ fil }) => {
+    const { user } = useContext(AuthContext)
+
+    const [card, setCard] = useState(fil)
     const { title, description, marks, photoURL, category, _id } = fil
+    
+
+    
+    const handleDelete = async id => {
+        if (user?.email !== fil.email) return toast.error('Error')
+        if(!user) return toast.error('Error')
+        try {
+            const { data } = await axios.delete(
+                `http://localhost:9000/delete/${id}`
+            )
+            console.log(data)
+            toast.success('Assignment Delete Successfuly ')
+            const remaining = card.filter(users => users._id !== id)
+            setCard(remaining)
+           
+
+        } catch (err) {
+            console.log(err.message)
+
+        }
+        
+    }
     return (
         <div>
+
             <div className="card  bg-base-100 shadow-xl">
                 <figure className="px-10 pt-10">
                     <img src={photoURL} alt="" className="rounded-xl" />
@@ -26,18 +57,23 @@ const FilterAllAssignment = ({ fil }) => {
 
                     <div className="card-actions mt-3">
                         <button className="btn btn-sm border-1 border-green-300">Update</button>
-                        <button className="btn btn-sm border-1 border-red-300">Delete</button>
-                        
+                        <button onClick={() => handleDelete(_id)} className="btn btn-sm border-1 border-red-300">Delete</button>
+
                     </div>
                     <Link to={`/ditelspg/${_id}`}>
-                    <div className="flex justify-center items-center gap-2 ">
+                        <div className="flex justify-center items-center gap-2 ">
                             <span className="mb-1">View  </span>
                             <GrLinkNext />
                         </div>
                     </Link>
                 </div>
-                
+
             </div>
+
+
+
+            <Toaster position="top-center" />
+
         </div>
     );
 };
